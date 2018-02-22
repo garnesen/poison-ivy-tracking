884940c34d6ca22146458f2877f25189705bd785
Gunnar Arnesen <garnesen@vt.edu>
2018-02-22 13:43:29 -0500

    Base android app

---
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hci_capstone.poison_ivy_tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
