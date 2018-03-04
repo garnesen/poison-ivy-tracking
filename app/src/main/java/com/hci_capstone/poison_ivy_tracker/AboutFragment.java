991008cdb9cfa47aa7599de16fd0415e296c2fd4
Douglas Botello <bdougl5@vt.edu>
2018-03-04 00:37:46 -0500

    Updated minimum sdk from 15 to 23. Played around witt images in the about page. Nothing substantial yet

--- app/src/main/java/com/hci_capstone/poison_ivy_tracker/AboutFragment.java
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/AboutFragment.java
@@ -1,10 +1,18 @@
 package com.hci_capstone.poison_ivy_tracker;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.PaintDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 /**
  * Created by douglasbotello on 2/23/18.
  */
@@ -19,8 +27,11 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+
         return rootView;
     }
 }
+
+
 
 
