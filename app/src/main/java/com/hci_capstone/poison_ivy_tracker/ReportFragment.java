15e34177d132c5fa9848a906eb052c2b3fd085c6
Gunnar Arnesen <garnesen@vt.edu>
2018-03-11 19:49:11 -0400

    Created the report page

--- app/src/main/java/com/hci_capstone/poison_ivy_tracker/ReportFragment.java
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/ReportFragment.java
@@ -1,25 +1,52 @@
 package com.hci_capstone.poison_ivy_tracker;
 
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Interpolator;
 import android.widget.Button;
-import android.widget.RadioGroup;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-import org.w3c.dom.Text;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * Fragment of the bottom navigation bar that allows the user to submit a report.
+ *
+ * TODO: If the user takes a picture and closes the app without being submitted, that image is still saved.
+ * TODO: Add custom icon images.
+ */
 public class ReportFragment extends Fragment {
 
-    Button submit;
-    TextView ivyTypeTextView;
-    RadioGroup ivyType;
-    RadioGroup ivyPresence;
+    ToggleButton yes_button, no_button, creeping_button, climbing_button, shrub_button, camera_button;
+    Button submit_button;
+    ToggleButtonGroup question1Group, question2Group;
+
+    AnimatorSet question2Fade, question3Fade;
+    Interpolator forwardInterpolator, reverseInterpolator;
+
+    File currentImageFile;
+
+    final String AUTHORITY = "com.hci_capstone.poison_ivy_tracker.fileprovider";
 
     public ReportFragment() {
 
@@ -27,38 +54,292 @@ public class ReportFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_report, container, false);
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
 
-        submit = (Button) rootView.findViewById(R.id.report_submit);
-        ivyTypeTextView = (TextView) rootView.findViewById(R.id.report_ivy_type_text);
-        ivyType = (RadioGroup) rootView.findViewById(R.id.report_ivy_type_radio_group);
+        yes_button = (ToggleButton) view.findViewById(R.id.report_yes_button);
+        no_button = (ToggleButton) view.findViewById(R.id.report_no_button);
+        creeping_button = (ToggleButton) view.findViewById(R.id.report_creeping_button);
+        climbing_button = (ToggleButton) view.findViewById(R.id.report_climbing_button);
+        shrub_button = (ToggleButton) view.findViewById(R.id.report_shrub_button);
+        camera_button = (ToggleButton) view.findViewById(R.id.report_camera_button);
+        submit_button = (Button) view.findViewById(R.id.report_submit_button);
 
-        ivyPresence = (RadioGroup) rootView.findViewById(R.id.report_ivy_presence_radio_group);
-        ivyPresence.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        createFadeAnimations(view);
+
+        question1Group = new ToggleButtonGroup();
+        question1Group.addToggleButton(yes_button);
+        question1Group.addToggleButton(no_button);
+
+        question2Group = new ToggleButtonGroup();
+        question2Group.addToggleButton(creeping_button);
+        question2Group.addToggleButton(climbing_button);
+        question2Group.addToggleButton(shrub_button);
+        question2Group.setEnabled(false);
+
+        question1Group.setOnCheckedChangeListener(new ToggleButtonGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int id) {
-                boolean present = id == R.id.report_ivy_presence_yes;
-
-                if (present) {
-                    ivyType.setVisibility(View.VISIBLE);
-                    ivyTypeTextView.setVisibility(View.VISIBLE);
-                    submit.setEnabled(false);
+            public void onCheckedChanged(ToggleButton button, boolean isChecked) {
+                if (button == yes_button) {
+                    question2Group.setEnabled(true);
+                    submit_button.setEnabled(false);
+                    performFadeInFor(question2Fade);
                 }
-                else {
-                    ivyType.clearCheck();
-                    ivyType.setVisibility(View.INVISIBLE);
-                    ivyTypeTextView.setVisibility(View.INVISIBLE);
-                    submit.setEnabled(true);
+                else if (button == no_button) {
+                    if (question2Group.isEnabled()) {
+                        performFadeOutFor(question2Fade);
+                    }
+                    if (camera_button.isEnabled()) {
+                        performFadeOutFor(question3Fade);
+                    }
+
+                    question2Group.setEnabled(false);
+                    question2Group.clearCheck();
+
+                    camera_button.setEnabled(false);
+                    camera_button.setChecked(false);
+
+                    submit_button.setEnabled(true);
                 }
             }
         });
 
-        ivyType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        question2Group.setOnCheckedChangeListener(new ToggleButtonGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int id) {
-                submit.setEnabled(true);
+            public void onCheckedChanged(ToggleButton button, boolean isChecked) {
+
+                // Only perform the animation after the first selection in question 2.
+                if (!camera_button.isEnabled()) {
+                    camera_button.setEnabled(true);
+                    performFadeInFor(question3Fade);
+                    submit_button.setEnabled(true);
+                }
             }
         });
-        return rootView;
+
+        camera_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                // Check if the current image is null to prevent the camera from opening when
+                // returning to the report fragment.
+                if (isChecked && currentImageFile == null) {
+                    dispatchTakePictureIntent();
+                }
+                else if (!isChecked && currentImageFile != null) {
+                    deleteCurrentImage();
+                }
+            }
+        });
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String poisonIvyPresent = question1Group.getCurSelectedTag();
+                String poisonIvyType = question2Group.getCurSelectedTag();
+                String imgName = currentImageFile != null ? currentImageFile.getName() : null;
+
+                Log.v("IVY_REPORT", "Info submitted: " + poisonIvyPresent + " " + poisonIvyType + " " + imgName);
+                Toast.makeText(getContext(), "Report submitted!", Toast.LENGTH_SHORT).show();
+
+                reset();
+            }
+        });
+
+        return view;
+    }
+
+    /**
+     * Reset the view.
+     */
+    public void reset() {
+        currentImageFile = null;
+        submit_button.setEnabled(false);
+
+        question1Group.clearCheck();
+        question1Group.setEnabled(true);
+
+        question2Group.clearCheck();
+        if (question2Group.isEnabled()) {
+            question2Group.setEnabled(false);
+            performFadeOutFor(question2Fade);
+        }
+
+        camera_button.setChecked(false);
+        if (camera_button.isEnabled()) {
+            camera_button.setEnabled(false);
+            performFadeOutFor(question3Fade);
+        }
+    }
+
+    /**
+     * Perform a fade in animation for an AnimatorSet.
+     * @param as
+     */
+    private void performFadeInFor(AnimatorSet as) {
+        as.setInterpolator(forwardInterpolator);
+        as.start();
+    }
+
+    /**
+     * Perform a fade out animation for an AnimatorSet.
+     * @param as
+     */
+    private void performFadeOutFor(AnimatorSet as) {
+        as.setInterpolator(reverseInterpolator);
+        as.start();
+    }
+
+    /**
+     * Initialize the AnimatorSets for fading in the questions.
+     * @param view
+     */
+    private void createFadeAnimations(View view) {
+        question2Fade = new AnimatorSet();
+        question3Fade = new AnimatorSet();
+        forwardInterpolator = new ForwardInterpolator();
+        reverseInterpolator = new ReverseInterpolator();
+
+        // Get the animation time from the dimens.
+        TypedValue out = new TypedValue();
+        getResources().getValue(R.dimen.report_fade_animation_duration, out, true);
+        int animationDuration = (int) out.getFloat();
+
+        // Create the fade animation for question 2.
+        TextView question2 = (TextView) view.findViewById(R.id.report_question_2);
+        TextView creeping_text = (TextView) view.findViewById(R.id.report_creeping_text);
+        TextView climbing_text = (TextView) view.findViewById(R.id.report_climbing_text);
+        TextView shrub_text = (TextView) view.findViewById(R.id.report_shrub_text);
+        View divider2 = (View) view.findViewById(R.id.divider2);
+
+        question2Fade.playTogether(
+                createSingleFadeAnimation(question2, animationDuration),
+                createSingleFadeAnimation(creeping_text, animationDuration),
+                createSingleFadeAnimation(climbing_text, animationDuration),
+                createSingleFadeAnimation(shrub_text, animationDuration),
+                createSingleFadeAnimation(divider2, animationDuration),
+                createSingleFadeAnimation(creeping_button, animationDuration),
+                createSingleFadeAnimation(climbing_button, animationDuration),
+                createSingleFadeAnimation(shrub_button, animationDuration)
+        );
+
+        // Create the fade animation for question 3.
+        TextView question3 = (TextView) view.findViewById(R.id.report_question_3);
+        TextView camera_text = (TextView) view.findViewById(R.id.report_camera_text);
+        View divider3 = (View) view.findViewById(R.id.divider3);
+
+        question3Fade.playTogether(
+                createSingleFadeAnimation(question3, animationDuration),
+                createSingleFadeAnimation(camera_text, animationDuration),
+                createSingleFadeAnimation(divider3, animationDuration),
+                createSingleFadeAnimation(camera_button, animationDuration)
+        );
+
+    }
+
+    /**
+     * Create an alpha transform Animator for a given view and duration.
+     * @param view
+     * @param duration
+     * @return the animation
+     */
+    private Animator createSingleFadeAnimation(View view, int duration) {
+        Animator a = ObjectAnimator.ofFloat(view, "alpha", view.getAlpha(), 1.0f);
+        a.setDuration(duration);
+        return a;
+    }
+
+    /**
+     * An interpolator that reverses the effects of an animation.
+     */
+    public class ReverseInterpolator implements Interpolator {
+        @Override
+        public float getInterpolation(float paramFloat) {
+            return Math.abs(paramFloat - 1f);
+        }
+    }
+
+    /**
+     * An interpolator for setting an animation back to forward.
+     */
+    public class ForwardInterpolator implements Interpolator {
+        @Override
+        public float getInterpolation(float paramFloat) {
+            return paramFloat;
+        }
+    }
+
+    /************************
+     *    CAMERA CONTROL    *
+     ************************/
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    /**
+     * Starts the default camera app and saves the image in internal storage.
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                if (currentImageFile != null) {
+                    deleteCurrentImage();
+                }
+                photoFile = createImageFile();
+            }
+            catch (IOException e) {
+                Log.v("IVY_IMAGE_CAPTURE", "Failed to create photo file.");
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(), AUTHORITY, photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Log.v("IVY_IMAGE_CAPTURE", "Successfully took picture.");
+        }
+        else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_CANCELED){
+            Log.v("IVY_IMAGE_CAPTURE", "Picture was cancelled.");
+
+            // Delete the empty temp file.
+            deleteCurrentImage();
+
+            camera_button.setChecked(false);
+        }
+    }
+
+    /**
+     * Creates an image file in internal storage.
+     * @return the created image file
+     * @throws IOException
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getFilesDir();
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentImageFile = image;
+        return image;
+    }
+
+    /**
+     * Delete the current file if it exists.
+     */
+    private void deleteCurrentImage() {
+        if (currentImageFile != null) {
+            currentImageFile.delete();
+            currentImageFile = null;
+        }
     }
 }
---
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/ToggleButtonGroup.java
@@ -0,0 +1,118 @@
+package com.hci_capstone.poison_ivy_tracker;
+
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class that takes toggle buttons and mimics the behavior of a RadioButtonGroup.
+ */
+public class ToggleButtonGroup {
+
+    private List<ToggleButton> buttons;
+    private OnCheckedChangeListener listener;
+    private boolean isEnabled;
+    private ToggleButton curSelected;
+
+    /**
+     * Creates a new ToggleButtonGroup.
+     */
+    public ToggleButtonGroup() {
+        buttons = new ArrayList<>();
+        isEnabled = true;
+    }
+
+    /**
+     * Adds a toggle button to the group.
+     * @param button
+     */
+    public void addToggleButton(final ToggleButton button) {
+        buttons.add(button);
+        button.setChecked(false);
+
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                // Toggle the rest of the buttons off.
+                if (isChecked) {
+                    button.setEnabled(false);
+                    curSelected = button;
+                    for (ToggleButton curButton : buttons) {
+                        if (curButton != button) {
+                            curButton.setChecked(false);
+                            curButton.setEnabled(true);
+                        }
+                    }
+                }
+
+                // Pass the event on to listeners.
+                if (listener != null && isChecked) {
+                    listener.onCheckedChanged(button, isChecked);
+                }
+            }
+        });
+    }
+
+    /**
+     * Set the enabled state of all the buttons in the group.
+     * @param enabled
+     */
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+        for (ToggleButton button : buttons) {
+            button.setEnabled(enabled);
+        }
+    }
+
+    /**
+     * Returns true if the ToggleButtonGroup is enabled.
+     * @return true if enabled, false otherwise
+     */
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    /**
+     * Sets each ToggleButton to be unchecked.
+     */
+    public void clearCheck() {
+        for (ToggleButton button : buttons) {
+            button.setChecked(false);
+        }
+        curSelected = null;
+    }
+
+    /**
+     * Get the currently selected ToggleButton.
+     * @return the ToggleButton
+     */
+    public ToggleButton getCurSelected() {
+        return curSelected;
+    }
+
+    /**
+     * Get the tag of the current selected item.
+     * @return the tag
+     */
+    public String getCurSelectedTag() {
+        return curSelected != null ? (String) curSelected.getTag() : null;
+    }
+
+    /**
+     * Set the listener for when a ToggleButton in the group has been changed.
+     * @param listener
+     */
+    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
+        this.listener = listener;
+    }
+
+    /**
+     * A basic listener interface.
+     */
+    public interface OnCheckedChangeListener {
+        void onCheckedChanged(ToggleButton button, boolean isChecked);
+    }
+}
