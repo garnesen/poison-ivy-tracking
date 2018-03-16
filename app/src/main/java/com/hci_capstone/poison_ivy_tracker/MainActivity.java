93d4bcc1f2cdc02c1c1da5da6b18881e4a70d894
Gunnar Arnesen <garnesen@vt.edu>
2018-03-16 16:40:15 -0400

    Add SimpleLocation library and get location permission

--- app/src/main/java/com/hci_capstone/poison_ivy_tracker/MainActivity.java
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hci_capstone.poison_ivy_tracker;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +14,16 @@ import android.view.MenuItem;
 import java.util.ArrayList;
 import java.util.List;
 
+import im.delight.android.location.SimpleLocation;
+
+// TODO: Create an error page if the user denies location services.
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private List<Fragment> fragments;
+
+    private SimpleLocation location;
+    final int REQUEST_LOCATION = 200;
 
     enum FragmentTag {
         REPORT, IDENTIFY, LEADERBOARDS, ABOUT, SETTINGS
@@ -30,6 +39,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // New SimpleLocation object, easy access to latitude and longitude.
+        location = new SimpleLocation(this);
+
+        // Check for location permissions.
+        if (!hasLocationPermissions()) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        }
+        else {
+            location.beginUpdates();
+        }
+
+        // If location is not turned on...
+        if (!location.hasLocationEnabled()) {
+            // ...ask the user to enable location access.
+            SimpleLocation.openSettings(this);
+        }
+        
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -90,5 +116,42 @@ public class MainActivity extends AppCompatActivity {
         fragments.add(leaderboardsFragment);
         fragments.add(aboutFragment);
         fragments.add(settingsFragment);
+    }
+
+    /**
+     * Check if we have access to user location.
+     * @return true if we have access, false otherwise
+     */
+    private boolean hasLocationPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Make the device update its location.
+        if (hasLocationPermissions()) {
+            location.beginUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // Stop location updates (saves battery).
+        if (hasLocationPermissions()) {
+            location.endUpdates();
+        }
+
+        super.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                location.beginUpdates();
+            }
+        }
     }
 }
