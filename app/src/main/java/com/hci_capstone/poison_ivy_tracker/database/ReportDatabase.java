fc88a2ce53f53f401ff2c3ce19ec25334907c039
Gunnar Arnesen <garnesen@vt.edu>
2018-03-17 16:37:43 -0400

    Make the database a singleton and add async tasks for inserts

--- app/src/main/java/com/hci_capstone/poison_ivy_tracker/database/ReportDatabase.java
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/database/ReportDatabase.java
@@ -1,11 +1,56 @@
 package com.hci_capstone.poison_ivy_tracker.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+import android.os.AsyncTask;
 
 @Database(entities = {Report.class}, version = 1, exportSchema = false)
 @TypeConverters({DateTypeConverter.class})
 public abstract class ReportDatabase extends RoomDatabase {
+
+    private static ReportDatabase INSTANCE;
+
     public abstract ReportDAO reportDAO();
+
+    /**
+     * Get the database as a singleton as it is expensive.
+     * @param context
+     * @return the database
+     */
+    public static ReportDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context, ReportDatabase.class, "reports-database").build();
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Empty the instance.
+     */
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+
+    /**
+     * Insert reports into the database.
+     * @param reports
+     */
+    public void insertReports(Report... reports) {
+        new AddReportsInBackground().execute(reports);
+    }
+
+    /**
+     * An async task to insert reports into the database in the background.
+     */
+    private class AddReportsInBackground extends AsyncTask<Report, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Report... reports) {
+            INSTANCE.reportDAO().insertReports(reports);
+            return true;
+        }
+    }
 }
