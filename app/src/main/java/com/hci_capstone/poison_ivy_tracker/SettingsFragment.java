34960acb17fa9b9d9ff8829536b0cf4f6e832736
Gunnar Arnesen <garnesen@vt.edu>
2018-03-26 17:38:04 -0400

    Switch settings page to use PreferenceFragmentCompat

--- app/src/main/java/com/hci_capstone/poison_ivy_tracker/SettingsFragment.java
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/SettingsFragment.java
@@ -1,54 +1,34 @@
 package com.hci_capstone.poison_ivy_tracker;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
 
+import com.hci_capstone.poison_ivy_tracker.R;
 
-/**
- * Created by douglasbotello on 3/23/18.
- */
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-public class SettingsFragment extends Fragment {
-
-    public SettingsFragment() {
-
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Load the preferences from an XML resource
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        String[] settingsList = {"Trail Name (optional)", "Email Address (optional)"};
-
-        String trailName;
-        if (getResources().getString(R.string.trailname).equals("")) {
-            trailName = "No Trailname Set";
-        } else {
-            trailName = getResources().getString(R.string.trailname);
-        }
-        String email;
-        if (getResources().getString(R.string.trailname).equals("")) {
-            email = "No Email Set";
-        } else {
-            email = getResources().getString(R.string.email);
-        }
-        String[] settingsList2 = {trailName, email};
-
-        CustomList adapter = new CustomList(getActivity(), settingsList, settingsList2);
-
-        ListView listView = (ListView) rootView.findViewById(R.id.SettingsList);
-        listView.setAdapter(adapter);
-
-        return rootView;
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // TODO: Send notification to main activity to sync new changes.
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
 
-
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
 }
