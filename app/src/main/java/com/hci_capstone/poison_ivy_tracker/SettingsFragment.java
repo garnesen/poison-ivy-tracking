2f8188afd76afb1a22d3bd7b66345876a74a03c8
Gunnar Arnesen <garnesen@vt.edu>
2018-04-02 15:24:35 -0400

    Add service to sync changed settings with server

--- app/src/main/java/com/hci_capstone/poison_ivy_tracker/SettingsFragment.java
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/SettingsFragment.java
@@ -1,10 +1,22 @@
 package com.hci_capstone.poison_ivy_tracker;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private final String LOG_TAG = "ITCHY_SETTINGS";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -14,7 +26,35 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // TODO: Send notification to main activity to sync new changes.
+        // Store data in intent.
+        Bundle data = new Bundle();
+        data.putString("pref_key", key);
+        data.putString("pref_data", sharedPreferences.getString(key, ""));
+
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(getContext().getApplicationContext()));
+        Job uploadJob = dispatcher.newJobBuilder()
+                // Set the JobService that will be called.
+                .setService(SettingsSyncService.class)
+                // Name the job.
+                .setTag(key + "_sync_service")
+                // This job will only execute once.
+                .setRecurring(false)
+                // Start between 0 and 60 seconds from now.
+                .setTrigger(Trigger.executionWindow(0, 60))
+                // Make sure this runs even after a reboot.
+                .setLifetime(Lifetime.FOREVER)
+                // Do not replace this job if another is made.
+                .setReplaceCurrent(true)
+                // Retry with exponential backoff.
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                // Only perform the job on unmetered network.
+                .setConstraints(Constraint.ON_UNMETERED_NETWORK)
+                // Send data through bundle.
+                .setExtras(data)
+                .build();
+
+        Log.v(LOG_TAG, "Attempting to schedule settings sync job.");
+        dispatcher.mustSchedule(uploadJob);
     }
 
     @Override
---
+++ app/src/main/java/com/hci_capstone/poison_ivy_tracker/SettingsSyncService.java
@@ -0,0 +1,84 @@
+package com.hci_capstone.poison_ivy_tracker;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SettingsSyncService extends JobService {
+
+    private final String LOG_TAG = "ITCHY_SETTINGS_SERVICE";
+
+    private final String pref_key = "pref_key";
+    private final String data_key = "pref_data";
+
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+        final String url = getApplicationContext().getString(R.string.server_url);
+        if (TextUtils.isEmpty(url)) {
+            Log.v(LOG_TAG, "Exiting Job: Server url is empty.");
+            return false;
+        }
+
+        // This will be ignored if the app is running.
+        InstanceID.init(getApplicationContext());
+        RequestHandler.init(getApplicationContext());
+
+        // Get values for request.
+        String uid = InstanceID.getInstance().getId();
+        String key = params.getExtras().getString(pref_key);
+        String data = params.getExtras().getString(data_key);
+
+        // Create the JSON.
+        JSONObject json = new JSONObject();
+        JSONObject setting_change = new JSONObject();
+        try {
+            setting_change.put(key, data);
+            json.put("uid", uid);
+            json.put("setting_change", setting_change);
+        } catch (JSONException e) {
+            Log.v(LOG_TAG, "Exiting Job: Failed to create JSON for request.");
+            return false;
+        }
+
+        // Create the request.
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.v(LOG_TAG, "Response: " + response.toString());
+                        Log.v(LOG_TAG, "Exiting Job: Successfully synced settings with server.");
+                        jobFinished(params, false);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v(LOG_TAG, "Rescheduling Job: Error response from request: " + error);
+                        jobFinished(params, false);
+                    }
+                }
+        );
+
+        Log.v(LOG_TAG, "Adding to request queue, attempting to sync " + key + ":" + data);
+        RequestHandler.getInstance().addToRequestQueue(request);
+        return true; // Return true if work is still going on.
+    }
+
+    @Override
+    public boolean onStopJob(final JobParameters params) {
+        Log.v(LOG_TAG, "Exiting Job: onStopJob called.");
+        return false; // Return true if this job should be retried.
+    }
+}
